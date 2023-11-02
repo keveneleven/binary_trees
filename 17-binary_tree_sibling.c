a2f362637535de8fdf07218e3f179d60923466dd
Mnqobi Donga <kevedonga1017@gmail.com>
2023-11-02 11:52:49 -0700

    Sibling node fixed

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,12 +7,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
 	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
+	parent = node->parent;
 	if (parent->left == NULL || parent->right == NULL)
 	{
 		return (NULL);
